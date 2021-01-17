e9c71dcb658f02cb706579802afc849b1a7205ec
Kyle Cordell <kcordell-git@outlook.com>
2021-01-17 00:36:35 -0600

    Corrected issues with webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.css$/i,
