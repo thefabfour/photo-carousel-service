fb66f4ccf92731ab0890f6b790d06e374b3c3282
Kyle Cordell <kcordell-git@outlook.com>
2021-01-27 17:10:02 -0600

    Fixed CSS loader webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,10 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]--[hash:base64:5]',
+              },
             },
           },
         ],
